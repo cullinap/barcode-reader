17ea725e152d247efebc6b674d298c9b28422537
cullinap <cullinap@gmail.com>
2020-04-13 23:07:48 -0400

    quick photo edit

--- temp.py
+++ temp.py
@@ -6,7 +6,7 @@ import cv2
 
 def main():
 
-	filename = 'images/board_game.png'
+	filename = 'images/barcode.jpg'
 	W = 300	
 	img = cv2.imread(filename, cv2.IMREAD_COLOR)
 	height, width, depth = img.shape
@@ -14,9 +14,9 @@ def main():
 	newX, newY = img.shape[1]*imgscale, img.shape[0]*imgscale
 	new_img = cv2.resize(img, (int(newX), int(newY)))
 	
-	cv2.imshow("Show by CV2",new_img)
+	cv2.imshow("image",new_img)
+	cv2.imwrite('images/barcode_sm.jpg',new_img)
 	cv2.waitKey(0)
-	cv2.imwrite('images/board_game_sm.png',new_img)
 
 if __name__ == '__main__':
 	main()
